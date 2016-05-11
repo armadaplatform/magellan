955da54b701fb6d38e0d3998ebaf46cf8420b196
Filip Wójciak <fwojciak@ganymede.eu>
2016-05-11 17:26:58 +0200

    Configure stats.

--- src/domains.py
+++ src/domains.py
@@ -1,9 +1,9 @@
-from hermes import get_configs
+from armada import hermes
 
 
 def get_domains_to_services():
     result = {}
-    domains_configs = get_configs('.')
+    domains_configs = hermes.get_configs('.')
     for config_name, domains in domains_configs.items():
         if config_name.startswith('domains-') and config_name.endswith('.json'):
             result.update(domains)
