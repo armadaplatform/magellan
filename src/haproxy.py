2bf73076e11aac41c73e329e08146eb8738ed75c
labuzm <marcin.labuz@gamedesire.eu>
2016-07-01 13:01:12 +0200

    add support for token based authorization

--- src/haproxy.py
+++ src/haproxy.py
@@ -8,8 +8,11 @@ import socket
 import traceback
 
 import requests
+from armada import hermes
 
 import remote
+
+auth_config = hermes.get_config('auth_config.json', {})
 
 
 def _is_ip(hostname):
@@ -199,11 +202,21 @@ class MainHaproxy(Haproxy):
     def restart(self):
         pass
 
+    @staticmethod
+    def get_headers():
+        headers = {}
+        auth_token = auth_config.get('token')
+        # if token was provided, let's introduce ourselves with it
+        # in case main-haproxy requires it.
+        if auth_token:
+            headers['Authorization'] = 'Token {}'.format(auth_token)
+        return headers
+
     def put_config(self, config):
         with open(self.config_path, 'w') as config_file:
             config_file.write(config)
         address = self.load_balancer['address']
         url = 'http://{address}/upload_config'.format(**locals())
-        response = requests.post(url, data=base64.b64encode(config))
+        response = requests.post(url, data=base64.b64encode(config), headers=self.get_headers())
         if response.status_code != 200:
             raise Exception('upload_config http code: {status_code}'.format(status_code=response.status_code))
